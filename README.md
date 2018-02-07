2ba56956ee38f3146e81fae03e572263f3fbb11f
Robert Lord <robert@codepeak.se>
2018-02-07 19:07:57 +0100

    Fix code style

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # magento1-core
+
+This is the core Magento 1 library for modules developed by Codepeak AB. It holds general information reused within other modules.
--- app/code/local/Codepeak/Core/Helper/Data.php
+++ app/code/local/Codepeak/Core/Helper/Data.php
@@ -1,4 +1,12 @@
 <?php
+
+/**
+ * Class Codepeak_Core_Helper_Data
+ *
+ * @license  MIT License https://opensource.org/licenses/MIT
+ * @author   Robert Lord, Codepeak AB <robert@codepeak.se>
+ * @link     https://codepeak.se
+ */
 class Codepeak_Core_Helper_Data extends Mage_Core_Helper_Abstract
 {
 }
